8f8dbfffdd00608e5dd727f33cfcf4813ee96a3d
choiyo0322 <choiyo0322@gmail.com>
2023-10-11 23:31:48 +0900

    first commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -6,7 +6,9 @@ const form = welcome.querySelector("form");
 function handleRoomSubmit(event) {
    event.preventDefault();
    const input = form.querySelector("input"); 
-    socket.emit("enter_room",  {payload: input.value});
+    socket.emit("enter_room",  {payload: input.value}, () => {
+        console.log("server is done!");
+    });
    input.value = ""
 }
 
--- src/server.js
+++ src/server.js
@@ -14,9 +14,13 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-
-  socket.on("enter_room",(msg) => console.log(msg));
-
+  socket.on("enter_room",(msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      console.log(msg)
+      done();
+    }, 10000);
+  });
 });
 /* 
 const sockets = [];
